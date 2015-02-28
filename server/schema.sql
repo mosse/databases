68fe0d313d09c1eb5dec4ca22d2bf9e08e306f71
Mark Macdonald <mjemacdonald@gmail.com>
2015-02-27 16:47:20 -0800

    Initialized database and ported client files

--- server/schema.sql
+++ server/schema.sql
@@ -3,15 +3,28 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  username VARCHAR(60),
+  createdAt DATETIME,
+  message VARCHAR(140),
+  roomname VARCHAR(60)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  username VARCHAR(60),
+  avatarURL VARCHAR(200)
+);
+
+CREATE TABLE rooms (
+  roomname VARCHAR(60),
+  founder VARCHAR(60)
+);
+
+-- /* Create other tables and define schemas for them here! */
 
 
 
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+--   Execute this file from the command line by typing:
+--  *    mysql -u root < server/schema.sql
+--  *  to create the database and the tables.
 
